80695f3751ea43efb76bd94ecec0e75677c9f5d3
Ivan <pl_st@list.ru>
2023-06-04 00:00:25 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <users-table />
+  </div>
+</template>
+
+<script>
+  import UsersTable from "./components/UsersTable";
+
+  export default {
+    name: 'App',
+    components: {UsersTable},
+  }
+</script>
---
+++ src/components/UsersTable.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="users-table">
+    <header class="users-table__header users-table__row">
+      <h3>Имя</h3>
+      <h3>Телефон</h3>
+    </header>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "UsersTable"
+}
+</script>
+
+<style scoped>
+.users-table {
+  width: 100%;
+  padding: 40px;
+}
+
+.users-table__header {
+  display: flex;
+  justify-content: space-around;
+}
+
+.users-table__row > * {
+  width: 100%;
+  border: 1px solid red;
+  padding: 10px 20px;
+}
+</style>
